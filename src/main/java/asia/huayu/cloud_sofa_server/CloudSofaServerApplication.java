d341bf4fdab2d745a439330593975040a8708052
Rain-ziyu <1874300301@qq.com>
2023-01-06 11:01:55 +0800

    初始化提交

---
+++ src/main/java/asia/huayu/cloud_sofa_server/CloudSofaServerApplication.java
@@ -0,0 +1,13 @@
+package asia.huayu.cloud_sofa_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSofaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudSofaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/asia/huayu/cloud_sofa_server/CloudSofaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package asia.huayu.cloud_sofa_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudSofaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
